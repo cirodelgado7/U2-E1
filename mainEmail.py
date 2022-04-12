c8b8ad315355977d963fe8025a2e02dde8b304c7
cirodelgado7 <82001073+cirodelgado7@users.noreply.github.com>
2022-04-12 10:06:24 -0300

    Update mainEmail.py

--- mainEmail.py
+++ mainEmail.py
@@ -12,15 +12,27 @@ if __name__ == '__main__':
     print('\n Cambio de Contraseña')
     unEmail.cambiaContraseña()  # (Mensaje)
     print('\n Creación de Objetos de la clase Email')
-    Email.crearObjetoEmail('informatica.fcefn@gmail.com')
-    Email.crearObjetoEmail('wicc2019@unsj-cuim.edu')
-    Email.crearObjetoEmail('juanLiendro1957@yahoo.com')
+    E1 = Email()
+    E1.crearObjetoEmail('informatica.fcefn@gmail.com')
+    E2 = Email()
+    E2.crearObjetoEmail('wicc2019@unsj-cuim.edu')
+    E3 = Email()
+    E3.crearObjetoEmail('juanLiendro1957@yahoo.com')
     archivo = open('Correos.csv')
-    reader = csv.reader(archivo,delimiter=';')
-    d = input('\n Ingrese el dominio: ')
-    c=0
+    reader = csv.reader(archivo, delimiter=';')
+    d = input('\n Ingrese el Nombre de la Cuenta: ')
+    L = []
     for fila in reader:
-        if fila[2]==d:
-            c+=1
-    print('Cantidad de dominios iguales: {}'.format(c))
+        correo = fila[0] + fila[1] + fila[2] + "." + fila[3]
+        E4 = Email()
+        E4.crearObjetoEmail(correo)
+        L.append(E4)
+    c = 0
+    for i in range(len(L)):
+        if L[i].retornaEmail() == d:
+            c = c + 1
+    if (c > 1):
+        print('\n La cuenta ingresada está repetida')
+    else:
+        print('\n La cuenta ingresada no está repetida')
     archivo.close()
