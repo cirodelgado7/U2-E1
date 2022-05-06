c00672b5f64a735cbeebf14dc6f3e7b3b48e26c6
cirodelgado7 <82001073+cirodelgado7@users.noreply.github.com>
2022-05-06 19:43:27 -0300

    Update claseEmail.py

--- claseEmail.py
+++ claseEmail.py
@@ -1,4 +1,6 @@
+import csv
 import re
+
 
 class Email:  # (Definición de la Clase)
 
@@ -7,21 +9,31 @@ class Email:  # (Definición de la Clase)
     __tipodominio = ''  # (Atributo)
     __password = ''  # (Atributo)
 
-    def __init__(self, idcuenta = "", dominio = "", tipodominio = "", password = ""):  # (Constructor)
+    def __init__(self, idcuenta="", dominio="", tipodominio="", password=""):  # (Constructor)
         self.__idcuenta = idcuenta  # (Inicialización)
         self.__dominio = dominio  # (Inicialización)
         self.__tipodominio = tipodominio  # (Inicialización)
         self.__password = password  # (Inicialización)
 
     def retornaEmail(self):  # (Método)
-        return '' + self.__idcuenta + '@' + self.__dominio + '.' + self.__tipodominio
+        nombre = input('\n Nombre: ')  # Ingreso de datos por teclado
+        self.__idcuenta = input(' Nombre de Cuenta: ')  # Ingreso de datos por teclado
+        self.__dominio = input(' Dominio: ')  # Ingreso de datos por teclado
+        self.__tipodominio = input(' Tipo de Dominio: ')  # Ingreso de datos por teclado
+        self.__password = input(' Contraseña: ')  # Ingreso de datos por teclado
+        correo = self.__idcuenta + '@' + self.__dominio + '.' + self.__tipodominio
+        print('\n Estimado {} te enviaremos tus mensajes a la dirección {}'.format(nombre, correo))
 
     def getDominio(self):  # (Método)
-       return '' + self.__dominio
+        return self.__dominio
 
-    def cambiaContraseña(self):  # (Método)
+    def getIdCuenta(self):  # (Método)
+        return self.__idcuenta
+
+    def cambiaPassword(self):  # (Método)
+        print('\n Cambio de Contraseña')
         c = input('\n Contraseña actual: ')
-        if (c == self.__password):
+        if c == self.__password:
             n = input('\n Contraseña nueva: ')
             self.__password = n
             print('\n Contraseña cambiada con éxito')
@@ -29,7 +41,8 @@ class Email:  # (Definición de la Clase)
         else:
             print('\n Contraeña incorrecta')
 
-    def crearObjetoEmail(self, correo):
+    def crearObjetoEmail(self, correo):  # (Método)
+        print('\n Creación de Objetos de la clase Email')
         if re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$', correo.lower()):
             print('\n ****Correo correcto****')
             li = correo.split("@")
@@ -42,8 +55,39 @@ class Email:  # (Definición de la Clase)
         else:
             print('\n *****Correo incorrecto*****')
 
-    def mostrarObjetoCreado(self):
+    def mostrarObjetoCreado(self):  # (Método)
         print('Nombre de Cuenta: {}'.format(self.__idcuenta))
         print('Dominio: {}'.format(self.__dominio))
         print('Tipo de Dominio: {}'.format(self.__tipodominio))
         print('Contraseña: {}'.format(self.__password))
+
+    def testEmail(self):  # (Método)
+        E1 = Email()  # (Instanciación del Objeto)
+        E1.crearObjetoEmail('informatica.fcefn@gmail.com')  # (Mensaje o Llamada al método)
+        E2 = Email()  # (Instanciación del Objeto)
+        E2.crearObjetoEmail('wicc2019@unsj-cuim.edu')  # (Mensaje o Llamada al método)
+        E3 = Email()  # (Instanciación del Objeto)
+        E3.crearObjetoEmail('juanLiendro1957@yahoo.com')  # (Mensaje o Llamada al método)
+
+    def abrirArchivo(self):
+        archivo = open('Correos.csv')
+        reader = csv.reader(archivo, delimiter=';')
+        L = []
+        for fila in reader:
+            correo = fila[0]
+            E4 = Email()
+            E4.crearObjetoEmail(correo)
+            L.append(E4)
+        archivo.close()
+        return L
+
+    def verificarEmail(self, L):
+        d = input('\n Ingrese el Nombre de la Cuenta: ')
+        c = 0
+        for i in range(len(L)):
+            if L[i].getIdCuenta() == d:
+                c = c + 1
+        if c == 1:
+            print('\n El Nombre de la Cuenta si está repetido')
+        else:
+            print('\n El Nombre de la cuenta no está repetido')
