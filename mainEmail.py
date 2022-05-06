3f978b7e4aa05ee074652294d200da08371c0a69
cirodelgado7 <82001073+cirodelgado7@users.noreply.github.com>
2022-05-06 19:44:25 -0300

    Update mainEmail.py

--- mainEmail.py
+++ mainEmail.py
@@ -1,38 +1,9 @@
-import csv
 from claseEmail import Email
 
 if __name__ == '__main__':
-    nombre = input('\n Nombre: ')  # Ingreso de datos por teclado
-    idCuenta = input(' Nombre de Cuenta: ')
-    dominio = input(' Dominio: ')
-    tipoDominio = input(' Tipo de Dominio: ')
-    password = input(' Contraseña: ')
-    unEmail = Email(idCuenta, dominio, tipoDominio, password)
-    print('\n Estimado {} te enviaremos tus mensajes a la dirección {}'.format(nombre, unEmail.retornaEmail()))
-    print('\n Cambio de Contraseña')
-    unEmail.cambiaContraseña()  # (Mensaje)
-    print('\n Creación de Objetos de la clase Email')
-    E1 = Email()
-    E1.crearObjetoEmail('informatica.fcefn@gmail.com')
-    E2 = Email()
-    E2.crearObjetoEmail('wicc2019@unsj-cuim.edu')
-    E3 = Email()
-    E3.crearObjetoEmail('juanLiendro1957@yahoo.com')
-    archivo = open('Correos.csv')
-    reader = csv.reader(archivo, delimiter=';')
-    d = input('\n Ingrese el Nombre de la Cuenta: ')
-    L = []
-    for fila in reader:
-        correo = fila[0] + fila[1] + fila[2] + "." + fila[3]
-        E4 = Email()
-        E4.crearObjetoEmail(correo)
-        L.append(E4)
-    c = 0
-    for i in range(len(L)):
-        if L[i].retornaEmail() == d:
-            c = c + 1
-    if (c > 1):
-        print('\n La cuenta ingresada está repetida')
-    else:
-        print('\n La cuenta ingresada no está repetida')
-    archivo.close()
+    unEmail = Email()  # (Instanciación de un Objeto Email)
+    unEmail.retornaEmail()  # (Mensaje o Llamada al método)
+    unEmail.cambiaPassword()  # (Mensaje o Llamada al método)
+    unEmail.testEmail()  # (Mensaje o Llamada al método)
+    lista = unEmail.abrirArchivo()  # (Mensaje o Llamada al método)
+    unEmail.verificarEmail(lista)  # (Mensaje o Llamada al método)
